112fdcb4a77ff1bc78ecf119cebc6e9e458f5684
김민채252376 <mashu1751@kookmin.ac.kr>
2026-08-09 16:43:41 +0900

    firebase 테스트

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,49 @@
 from fastapi import FastAPI
+from app.firebase import db
 
 app = FastAPI()
 
+
 @app.get("/")
 def root():
-    return {"message":"Hello"}
+    return {"message": "Hello"}
+
+
+@app.get("/test/firebase")
+def test_firebase():
+
+    db.collection("students").document("test_student").set({
+        "badgeCount": 0,
+        "collectionCount": 0,
+        "joinedAt": "2023-01-01",
+        "level": 0,
+        "nickname": "테스트학생",
+        "xp": 0
+    })
+
+    return {
+        "success": True,
+        "message": "Firestore에 저장했습니다."
+    }
+
+
+@app.get("/test/firebase/read")
+def read_firebase():
+
+    doc = (
+        db.collection("students")
+        .document("test_student")
+        .get()
+    )
+
+    if not doc.exists:
+        return {
+            "success": False,
+            "message": "학생이 없습니다."
+        }
+
+    return {
+        "success": True,
+        "studentId": doc.id,
+        "data": doc.to_dict()
+    }
