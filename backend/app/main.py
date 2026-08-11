05b90f1c8a75b58784c4dcaed8c2a55919eba98c
김민채252376 <mashu1751@kookmin.ac.kr>
2026-08-11 23:47:46 +0900

    student(자기 정보 가져오기)api 구현

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
 
 from app.routers.auth import router as auth_router
+from app.routers.student import router as student_router
+
 
 app = FastAPI()
 
+
 app.include_router(auth_router)
+app.include_router(student_router)
 
 
 @app.get("/")
