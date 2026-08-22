811d4ebd3eb5158b000d5dd1c87f26e5c58d34e1
Sung-Won-AI <sbae7071@gmail.com>
2026-08-22 21:31:49 +0900

    Delete backend directory

--- backend/app/firebase.py
+++
@@ -1,9 +0,0 @@
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
-
-cred = credentials.Certificate("firebase_key.json")
-
-firebase_admin.initialize_app(cred)
-
-db = firestore.client()
--- backend/app/schemas/auth.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel, Field
-
-
-class StudentJoinRequest(BaseModel):
-    classCode: int = Field(ge=100000, le=999999)
-    nickname: str = Field(min_length=1, max_length=20)
